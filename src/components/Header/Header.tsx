4af72f29495de66fc31b6a96557ae233e77f7d65
Noah <noah.harborth.private@gmail.com>
2025-05-18 22:12:16 +0200

    add react-focus-lock; update Header component with delayed menu visibility; enhance styles for mobile resume button; replace old image assets

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,26 +7,63 @@ import Logo from '../../assets/images/noah_clay_transparent.png';
 import Resume from '../../assets/documents/NoahHarborthResume.pdf';
 import { ReactComponent as DocumentDownloadIcon } from '../../assets/images/icons/document-download.svg';
 import { ReactComponent as MenuIcon } from '../../assets/images/icons/squiggly-arrow-up.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ReactFocusLock from 'react-focus-lock';
 
-function HeaderNav({ isMobile }: { isMobile?: boolean }) {
+function HeaderNav({
+	isMobile,
+	delayedIsMenuOpen,
+}: {
+	isMobile?: boolean;
+	delayedIsMenuOpen?: boolean;
+}) {
 	return isMobile ? (
-		<nav className={`header__nav--mobile fade-in`}>
+		<nav className={`header__nav--mobile`}>
 			<ul className='header__list--mobile'>
 				<li className='header__item--mobile'>
-					<Link isNavLink to='/'>
+					<span
+						style={{
+							opacity: 0,
+							display: delayedIsMenuOpen ? 'none' : 'block',
+						}}
+					>
 						home
-					</Link>
+					</span>
+					{delayedIsMenuOpen && (
+						<Link isNavLink to='/'>
+							<span className='fade-in delay'>home</span>
+						</Link>
+					)}
 				</li>
 				<li className='header__item--mobile'>
-					<Link isNavLink to='/about'>
+					<span
+						style={{
+							opacity: 0,
+							display: delayedIsMenuOpen ? 'none' : 'block',
+						}}
+					>
 						about
-					</Link>
+					</span>
+					{delayedIsMenuOpen && (
+						<Link isNavLink to='/about'>
+							<span className='fade-in delay-03'>about</span>
+						</Link>
+					)}
 				</li>
 				<li className='header__item--mobile'>
-					<Link isNavLink to='/contact'>
+					<span
+						style={{
+							opacity: 0,
+							display: delayedIsMenuOpen ? 'none' : 'block',
+						}}
+					>
 						contact
-					</Link>
+					</span>
+					{delayedIsMenuOpen && (
+						<Link isNavLink to='/contact'>
+							<span className='fade-in delay-05'>contact</span>
+						</Link>
+					)}
 				</li>
 			</ul>
 		</nav>
@@ -53,7 +90,13 @@ function HeaderNav({ isMobile }: { isMobile?: boolean }) {
 	);
 }
 
-function HeaderResume({ isMobile }: { isMobile?: boolean }) {
+function HeaderResume({
+	isMobile,
+	delayedIsMenuOpen,
+}: {
+	isMobile?: boolean;
+	delayedIsMenuOpen?: boolean;
+}) {
 	return isMobile ? (
 		<div className={`header__resume--mobile fade-in`}>
 			<Button
@@ -62,8 +105,17 @@ function HeaderResume({ isMobile }: { isMobile?: boolean }) {
 				href={Resume}
 				className='header__resume-btn--mobile'
 			>
-				<DocumentDownloadIcon />
-				<span>download resume</span>
+				<span
+					style={{ opacity: 0, display: delayedIsMenuOpen ? 'none' : 'block' }}
+				>
+					download resume
+				</span>
+				{delayedIsMenuOpen && (
+					<span className='fade-slide-in--bottom delay-07'>
+						<DocumentDownloadIcon />
+						<span>download resume</span>
+					</span>
+				)}
 			</Button>
 		</div>
 	) : (
@@ -86,46 +138,82 @@ const CloseIcon = () => <span>✕</span>;
 
 function ExpandingMenuButton() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [delayedIsMenuOpen, setdelayedIsMenuOpen] = useState(false);
 
-  // TODO: also close Menu as soon as you either click outside or you click one of the nav items
+	useEffect(() => {
+		const timerId = setTimeout(
+			() => {
+				setdelayedIsMenuOpen(isMenuOpen);
+			},
+			isMenuOpen ? 500 : 100
+		);
+
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [isMenuOpen]);
+
+	useEffect(() => {
+		const handleClickOutsideToggleMenu = (event: MouseEvent) => {
+			const target = event.target as HTMLElement;
+			const menuButton = document.querySelector(
+				'.header__menu-icon-button'
+			) as HTMLElement;
+			const menuButtonIcon = document.querySelector(
+				'.header__menu-icon-button-icon'
+			) as HTMLElement;
+
+			if (target === menuButtonIcon || target === menuButton) {
+				return;
+			}
+
+			setIsMenuOpen(false);
+		};
+
+		document.addEventListener('click', handleClickOutsideToggleMenu);
+		return () => {
+			document.removeEventListener('click', handleClickOutsideToggleMenu);
+		};
+	}, []);
+
 	const handleToggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
 	};
 
-
-  // TODO: animate the menu nav items animating in/out and then after delay the menu closes
 	return (
-		<div
-			className={`header__expanding-menu-shell fade-in delay-15 ${
-				isMenuOpen
-					? 'header__expanding-menu-shell--open'
-					: 'header__expanding-menu-shell--closed'
-			}`}
-		>
+		<ReactFocusLock disabled={!isMenuOpen}>
 			<div
-				className={`header__menu-actual-content ${
-					isMenuOpen ? 'header__menu-actual-content--open' : ''
+				className={`header__expanding-menu-shell fade-in delay-15 ${
+					isMenuOpen
+						? 'header__expanding-menu-shell--open'
+						: 'header__expanding-menu-shell--closed'
 				}`}
-				id='header__mobile-menu-content-area'
 			>
-				<HeaderNav isMobile={true} />
-				<HeaderResume isMobile={true} />
-			</div>
+				<div
+					className={`header__menu-actual-content ${
+						isMenuOpen ? 'header__menu-actual-content--open' : ''
+					}`}
+					id='header__mobile-menu-content-area'
+				>
+					<HeaderNav isMobile={true} delayedIsMenuOpen={delayedIsMenuOpen} />
+					<HeaderResume isMobile={true} delayedIsMenuOpen={delayedIsMenuOpen} />
+				</div>
 
-			{/* TODO: Make icon button as its own button component version */}
-			<Button
-				type='round'
-				className='header__menu-icon-button'
-				onClick={handleToggleMenu}
-				aria-expanded={isMenuOpen}
-				aria-controls='header__mobile-menu-content-area'
-				aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-			>
-				<span className='icon-wrapper'>
-					{isMenuOpen ? <CloseIcon /> : <MenuIcon />}
-				</span>
-			</Button>
-		</div>
+				{/* TODO: Make icon button as its own button component version */}
+				<Button
+					type='round'
+					className='header__menu-icon-button'
+					onClick={handleToggleMenu}
+					aria-expanded={isMenuOpen}
+					aria-controls='header__mobile-menu-content-area'
+					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+				>
+					<span className='header__menu-icon-button-icon'>
+						{isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+					</span>
+				</Button>
+			</div>
+		</ReactFocusLock>
 	);
 }
 
